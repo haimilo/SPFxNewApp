962a1b4272298da754f41134658a9e05b24cd4aa
HarryNhu <harry.nhu@vmodev.com>
2023-02-10 00:29:16 +0700

    first commit

---
+++ src/webparts/faq/components/IFaqProps.ts
@@ -0,0 +1,9 @@
+export interface IFaqProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+  message: string;
+  displayName: string;
+}
